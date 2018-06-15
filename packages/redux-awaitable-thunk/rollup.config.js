92adc3f357717d88bc8d243c9c614262a933df56
Ian Taylor <ian.b.taylor@gmail.com>
2018-06-15 15:15:15 -0700

    Add awaitable thunks middleware

---
+++ packages/redux-awaitable-thunk/rollup.config.js
@@ -0,0 +1,24 @@
+import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/awaitableThunkMiddleware.js',
+    external: ['url-mapper', 'querystring'],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs', exports: 'named' },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
+];
---
+++ packages/redux-awaitable-thunk/src/awaitableThunkMiddleware.test.js
@@ -0,0 +1,122 @@
+import { createStore, applyMiddleware } from 'redux';
+import awaitableThunkMiddleware, { awaitableThunk, inProgress, after, resetThunkState, afterExactly } from './awaitableThunkMiddleware.js';
+import thunkMiddleware from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(awaitableThunkMiddleware, thunkMiddleware)(createStore);
+
+const namedSimpleThunk = () => awaitableThunk('namedSimpleThunk', async (dispatch) => {
+  await sleep(50);
+  dispatch({ type: 'action1' });
+});
+
+describe('awaitableThunks', () => {
+  afterEach(() => {
+    resetThunkState();
+  });
+
+  test('await an inProgress thunk', async () => {
+    const store = createStoreWithMiddleware(simpleReducer);
+    const listener = jest.fn();
+    store.subscribe(listener);
+    store.dispatch(namedSimpleThunk());
+    store.dispatch(async (dispatch) => {
+      await inProgress('namedSimpleThunk');
+      dispatch({ type: 'action2' });
+    });
+    await sleep(60);
+    expect(store.getState().inOrderActions).toEqual(['action1', 'action2']);
+    expect(listener.mock.calls.length).toBe(2);
+  });
+
+  test('using inProgress on a thunk that is not in progress resolves immediately', async () => {
+    const store = createStoreWithMiddleware(simpleReducer);
+    const listener = jest.fn();
+    store.subscribe(listener);
+    await store.dispatch(async (dispatch) => {
+      await inProgress('namedSimpleThunk');
+      dispatch({ type: 'action2' });
+    });
+    expect(store.getState().inOrderActions).toEqual(['action2']);
+    expect(listener.mock.calls.length).toBe(1);
+  });
+
+  test('await a named thunk using "after"', async () => {
+    const store = createStoreWithMiddleware(simpleReducer);
+    const listener = jest.fn();
+    store.subscribe(listener);
+    store.dispatch(async (dispatch) => {
+      await after('namedSimpleThunk');
+      dispatch({ type: 'action2' });
+    });
+    store.dispatch(namedSimpleThunk());
+    await sleep(60);
+    expect(store.getState().inOrderActions).toEqual(['action1', 'action2']);
+    expect(listener.mock.calls.length).toBe(2);
+  });
+
+  test('"after" works even if named thunk has been run multiple times', async () => {
+    const store = createStoreWithMiddleware(simpleReducer);
+    const listener = jest.fn();
+    store.subscribe(listener);
+    await store.dispatch(namedSimpleThunk());
+    await store.dispatch(namedSimpleThunk());
+    await store.dispatch(async (dispatch) => {
+      await after('namedSimpleThunk');
+      dispatch({ type: 'action2' });
+    });
+    expect(store.getState().inOrderActions).toEqual(['action1', 'action1', 'action2']);
+    expect(listener.mock.calls.length).toBe(3);
+  });
+
+  test('using "after", awaited thunk fires if named thunk was run before', async () => {
+    const store = createStoreWithMiddleware(simpleReducer);
+    const listener = jest.fn();
+    store.subscribe(listener);
+    await store.dispatch(namedSimpleThunk());
+    await store.dispatch(namedSimpleThunk());
+    await store.dispatch(async (dispatch) => {
+      await after('namedSimpleThunk');
+      dispatch({ type: 'action2' });
+    });
+    expect(store.getState().inOrderActions).toEqual(['action1', 'action1', 'action2']);
+    expect(listener.mock.calls.length).toBe(3);
+  });
+
+  test('"afterExactly" runs only when the number of times the thunk has been called matches the requested number of calls', async () => {
+    const store = createStoreWithMiddleware(simpleReducer);
+    const listener = jest.fn();
+    store.subscribe(listener);
+    store.dispatch(async (dispatch) => {
+      await afterExactly('namedSimpleThunk', 3);
+      dispatch({ type: 'action2' });
+    });
+    await store.dispatch(namedSimpleThunk());
+    await store.dispatch(namedSimpleThunk());
+    await store.dispatch(namedSimpleThunk());
+    await store.dispatch(namedSimpleThunk());
+    expect(store.getState().inOrderActions).toEqual(['action1', 'action1', 'action1', 'action2', 'action1']);
+    expect(listener.mock.calls.length).toBe(5);
+  });
+});
+
+
+// const thunkAwaitsAnotherThunk = () => async (getState, dispatch) => {
+//   await dispatch(simpleThunk());
+//   await dispatch({ type: 'action2' });
+// };
+
+// const awaitableThunkAwaitsAnotherThunk = awaitableThunk('awaitableThunkAwaitsAnotherThunk', thunkAwaitsAnotherThunk);
+
+
+function simpleReducer(state = {
+  inOrderActions: [],
+}, action) {
+  if (action.type.startsWith('action')) {
+    state.inOrderActions.push(action.type);
+  }
+  return state;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
