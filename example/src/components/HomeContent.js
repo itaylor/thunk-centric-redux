27699ac778627a8a72efc70b0f3f698a365aa679
Ian Taylor <ian.b.taylor@gmail.com>
2019-06-13 21:40:16 -0700

    Add note that example is incomplete to docs

--- example/src/components/HomeContent.js
+++ example/src/components/HomeContent.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function HomeContent() {
   return <div>
     <p>
-      This example app uses all of the middleware that are part of the 'thunk-centric-redux' repository.
+      This example (if I ever completed it) would use all of the middleware that are part of the 'thunk-centric-redux' repository.
     </p>
     <p>
       It's meant to showcase the way that things are connected to each other, and seeks to be as simplistic and stripped-down as possible.
