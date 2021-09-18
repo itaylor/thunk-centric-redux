61da4ecf06d3aadacfb38570035d21da482f919a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-18 11:06:32 +0800

    Delete packages directory

--- packages/redux-socket.io-error-thunk/src/MockStore.js
+++
@@ -1,8 +0,0 @@
-export default class MockStore {
-  constructor(initialState = {}) {
-    this.dispatch = jest.fn();
-    this.subscribe = jest.fn();
-    this.getState = jest.fn(() => initialState);
-    this.replaceReducer = jest.fn();
-  }
-}
--- packages/socket.io-promise/src/MockSocket.js
+++
@@ -1,5 +0,0 @@
-module.exports = class MockSocket {
-  constructor() {
-    this.emit = jest.fn();
-  }
-};
